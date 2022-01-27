fbfb197f14d5ac5d85184b49cb4ebf538884975c
pablomdd <pablo.mdd.007@gmail.com>
2022-01-27 14:48:31 -0600

    Clear comments, and add kp val

--- cart/encoder_speed_control_PID/encoder_speed_control_PID.ino
+++ cart/encoder_speed_control_PID/encoder_speed_control_PID.ino
@@ -3,24 +3,32 @@
 String inputString = "";
 bool stringComplete = false;
 
-const byte C1 = 3; // Entrada de la señal A del encoder (Cable amarillo).
-const byte C2 = 2; // Entrada de la señal B del encoder (Cable verde).
+// Entrada de la señal A del encoder (Cable amarillo).
+const byte C1 = 3;
+// Entrada de la señal B del encoder (Cable verde).
+const byte C2 = 2;
 //  Motor DC
 const byte in1 = 6;
 const byte in2 = 7;
 const byte ena = 10;
-
+// Pulsos
 volatile int n = 0;
 volatile byte ant = 0;
 volatile byte act = 0;
 
-double N = 0.0; // Revoluciones por minuto calculadas.
-int Cv = 0; // Variable de control (pwm)
-unsigned long lastTime = 0; // Tiempo anterior
-unsigned long sampleTime = 500; // Tiempo de muestreo
-unsigned int R = 1980; // Número de muescas que tiene el disco del encoder.
+// Revoluciones por minuto calculadas.
+double N = 0.0;
+// Variable de control (pwm)
+int Cv = 0;
+// Tiempo anterior
+unsigned long lastTime = 0;
+// Tiempo de muestreo
+unsigned long sampleTime = 500;
+// Número de muescas que tiene el disco del encoder.
+unsigned int R = 1980;
 
-volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+// specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+volatile int posi = 0;
 long prevT = 0;
 float eprev = 0;
 float eintegral = 0;
@@ -43,56 +51,60 @@ void setup()
   Serial.println("Velocidad RPM");
 }
 
-void loop() {
-//  if (stringComplete) {
-//    Cv = (double)inputString.toInt();
-//    inputString = "";
-//    stringComplete = false;
-//  }
+void loop()
+{
+  //  if (stringComplete) {
+  //    Cv = (double)inputString.toInt();
+  //    inputString = "";
+  //    stringComplete = false;
+  //  }
 
-//  int target = R;
- int target = R*sin(prevT/1e6);
- 
- // PID constants
+  //  int target = R;
+  int target = R * sin(prevT / 1e6);
+
+  // PID constants
   float kp = 1;
   float kd = 0.080;
   float ki = 0.0;
 
   // time difference
   long currT = micros();
-  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  float deltaT = ((float)(currT - prevT)) / (1.0e6);
   prevT = currT;
 
-  int pos = 0; 
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+  int pos = 0;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+  {
     pos = n;
-  }  
+  }
   // error
   int e = pos - target;
 
   // derivative
-  float dedt = (e-eprev)/(deltaT);
+  float dedt = (e - eprev) / (deltaT);
 
   // integral
-  eintegral = eintegral + e*deltaT;
+  eintegral = eintegral + e * deltaT;
 
   // control signal
-  float u = kp*e + kd*dedt + ki*eintegral;
+  float u = kp * e + kd * dedt + ki * eintegral;
 
-    // motor power
+  // motor power
   float pwr = fabs(u);
-  if( pwr > 255 ){
+  if (pwr > 255)
+  {
     pwr = 255;
   }
 
   // motor direction
   int dir = 1;
-  if(u<0){
+  if (u < 0)
+  {
     dir = -1;
   }
 
   // signal the motor
-  setMotor(dir,pwr,ena,in1,in2);
+  setMotor(dir, pwr, ena, in1, in2);
 
   // store previous error
   eprev = e;
@@ -101,8 +113,8 @@ void loop() {
   Serial.print(" ");
   Serial.print(pos);
   Serial.println();
-  
-//  computeRpm();
+
+  //  computeRpm();
 }
 
 //void serialEvent() {
@@ -115,57 +127,79 @@ void loop() {
 //  }
 //}
 
-void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
-  analogWrite(ena,pwmVal);
-  if(dir == 1){
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2)
+{
+  analogWrite(ena, pwmVal);
+  if (dir == 1)
+  {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
   }
-  else if(dir == -1){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
+  else if (dir == -1)
+  {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
   }
-  else{
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-  }  
+  else
+  {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  }
 }
 
-
-void computeRpm(void) {
-  if (millis() - lastTime >= sampleTime) { // Se actualiza cada segundo
+void computeRpm(void)
+{
+  // Se actualiza cada segundo
+  if (millis() - lastTime >= sampleTime)
+  {
     Serial.println(n);
-    N = (n * 60.0 * 1000.0) / ((millis() - lastTime) * R); // Calculamos las revoluciones por minuto
-    lastTime = millis(); // Almacenamos el tiempo actual.
-    n = 0; // Inicializamos los pulsos.
+    // Calculamos las revoluciones por minuto
+    N = (n * 60.0 * 1000.0) / ((millis() - lastTime) * R);
+    // Almacenamos el tiempo actual.
+    lastTime = millis();
+    // Inicializamos los pulsos.
+    n = 0;
     // Gira el motor en un sentido antihorario.
-    if (Cv > 0) {
+
+    if (Cv > 0)
+    {
       digitalWrite(in1, HIGH);
       digitalWrite(in2, LOW);
-    } else {
+    }
+    else
+    {
       // Gira el motor en un sentido horario.
       digitalWrite(in1, LOW);
       digitalWrite(in2, HIGH);
     }
-    analogWrite(ena, abs(Cv)); // Aplica señal de control (pwm).
-    Serial.print("RPM: "); Serial.println(N);
+    // Aplica señal de control (pwm).
+    analogWrite(ena, abs(Cv));
+    Serial.print("RPM: ");
+    Serial.println(N);
   }
 }
 
-
 // Encoder precisión cuádruple.
-void encoder(void) {
+void encoder(void)
+{
   ant = act;
   act = PIND & 12;
 
-  if (ant == 0 && act == 4) n++;
-  if (ant == 4 && act == 12) n++;
-  if (ant == 8 && act == 0) n++;
-  if (ant == 12 && act == 8) n++;
+  if (ant == 0 && act == 4)
+    n++;
+  if (ant == 4 && act == 12)
+    n++;
+  if (ant == 8 && act == 0)
+    n++;
+  if (ant == 12 && act == 8)
+    n++;
 
-  if (ant == 0 && act == 8) n--;
-  if (ant == 4 && act == 0) n--;
-  if (ant == 8 && act == 12) n--;
-  if (ant == 12 && act == 4) n--;
-
+  if (ant == 0 && act == 8)
+    n--;
+  if (ant == 4 && act == 0)
+    n--;
+  if (ant == 8 && act == 12)
+    n--;
+  if (ant == 12 && act == 4)
+    n--;
 }
