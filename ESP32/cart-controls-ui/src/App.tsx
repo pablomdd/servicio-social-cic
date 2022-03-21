1402ac5665996b55100f69574512f8cf4bbcb155
pablomdd <pablo.mdd.007@gmail.com>
2022-03-21 12:39:07 -0600

    create react project

---
+++ ESP32/cart-controls-ui/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box w="100vw" h="100vh" bg="tomato">
+
+    </Box>
+  );
+}
+
+export default App;
